dda435ff6d217ef7ecde537692f58f0704eaa281
612847413 <brendan.muldoon@bt.com>
2022-07-13 11:05:16 +0100

    initial commit

---
+++ src/main/java/com/example/ansible/ansibleexample/AnsibleExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.ansible.ansibleexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnsibleExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnsibleExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ansible/ansibleexample/AnsibleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ansible.ansibleexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnsibleExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
